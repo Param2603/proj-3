cf3e95b2d8fad64ca92683b19e410125ab9cc3ef
Parth99001 <parthgandhi350@gmail.com>
2025-07-03 15:39:16 +0530

    form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-  // const[data,setData] = useState
+
   return (
-    <div className='' >
-        <h1 className='text-[30px] text-blue-700 text-center'>Login Form</h1>
+    <div>
+          <div className='px-5 border-1 w-100 py-5 ' >
+        <h1 className=' text-[30px] text-gray-900 font-bold font-serif  mb-10'>Login Form</h1>
         <div>
-            <input className='border-2 ' type="text" placeholder='Email Address' />
+            <input className='border-2 border-gray-300 hover:border-gray-400 mb-5 rounded-[7px] px-1 py-1' type="text" placeholder='Email Address' />
         </div>
         <div>
-            <input className='border-2' type="password" placeholder='Password' />
+            <input className='border-2 border-gray-300 hover:border-gray-400 mb-1 rounded-[7px] px-1 py-1' type="password" placeholder='Password' />
         </div>
-        <p>Forgot password?</p>
-        <button>Login</button>
-        <p>Not a member? Signup</p>
+        <p className='text-blue-500 font-semibold mb-3'>Forgot password?</p>
+        <button className='border-1 mb-5 font-mono px-18 py-1 text-white bg-blue-500 cursor-pointer rounded-[10px] hover:bg-blue-600'>Login</button>
+        <p className='font-semibold'>Not a member? <span className='text-blue-400'> Signup now</span></p>
     </div>
+    </div>
+
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup Form</h1>
+    <div className='px-5 border-1 w-100 py-5 '>
+      <h1 className='text-[30px] text-gray-900 font-bold font-serif  mb-10'>Signup Form</h1>
       <div>
-        <input type="text" placeholder='Email Address' />
+        <input className='border-2 border-gray-300 hover:border-gray-400 mb-5 rounded-[7px] px-1 py-1' type="text" placeholder='Email Address' />
       </div>
         <div>
-        <input type="text" placeholder='Password' />
+        <input className='border-2 border-gray-300 hover:border-gray-400 mb-5 rounded-[7px] px-1 py-1' type="text" placeholder='Password' />
       </div>
         <div>
-        <input type="text" placeholder='Confirm password' />
+        <input className='border-2  border-gray-300 hover:border-gray-400 mb-3 rounded-[7px] px-1 py-1' type="text" placeholder='Confirm password' />
       </div>
-      <button>Signup</button>
-      <p>Already have an account? Login</p>
+      <button className='border-1 mb-5 hover:bg-blue-600 font-mono px-18 py-1 text-white bg-blue-500 cursor-pointer rounded-[10px]'>Signup</button>
+      <p className='font-semibold'>Already have an account? <span className='text-white bg-blue-500 hover:bg-blue-600 font-mono cursor-pointer  border-1 px-2 py-1 rounded-[10px]'>Login</span></p>
     </div>
   )
 }
